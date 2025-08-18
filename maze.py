4a39f61d53d7796b640bb866b816ba74aab74d16
MaxM <m.mirnik@campus.tu-berlin.de>
2025-08-18 21:33:50 +0200

    Refactor maze generation logic and improve boundary checks

--- maze.py
+++ maze.py
@@ -1,24 +1,31 @@
 from random import randint
 
+def maze_pos(maze, pos):
+    return (maze[pos[0]][pos[1]])
+
+def maze_pos_in(maze, pos):
+    return (pos[0] >= 0 and pos[0] < len(maze) and pos[1] >= 0 and pos[1] < len(maze[0]))
+
 def create_maze(width, height):
-    maze = [[' ' for _ in range(width)] for _ in range(height)]
+    maze = [['#' for _ in range(width)] for _ in range(height)]
     center = (height // 2, width // 2)
     current_pos = center
     maze[center[0]][center[1]] = '0'
     directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # right, down, left, up
     n = 1
     while True:
-        dir = directions[randint(0, 2)]
+        dir = directions[randint(0, 3)]
         next_pos = (current_pos[0] + dir[0], current_pos[1] + dir[1])
         goal_pos = (next_pos[0] + dir[0], next_pos[1] + dir[1])
-        print(f"dir: {dir}, Current position: {current_pos}, Next position: {next_pos}, Goal position: {goal_pos}")
-        if goal_pos[0] >= 0 and goal_pos[0] < height and goal_pos[1] >= 0 and goal_pos[1] < width:
-            maze[next_pos[0]][next_pos[1]] = str(n)
-            maze[goal_pos[0]][goal_pos[1]] = str(n+1)
+        # print(f"dir: {dir}, Current position: {current_pos}, Next position: {next_pos}, Goal position: {goal_pos}")
+
+        if maze_pos_in(maze, goal_pos) and maze_pos(maze, goal_pos) != '0':
+            maze[next_pos[0]][next_pos[1]] = " "
+            maze[goal_pos[0]][goal_pos[1]] = " "
             n += 2
             current_pos = goal_pos
-        elif next_pos[0] >= 0 and next_pos[0] < height and next_pos[1] >= 0 and next_pos[1] < width:
-            maze[next_pos[0]][next_pos[1]] = str(n)
+        elif maze_pos_in(maze, next_pos):
+            maze[next_pos[0]][next_pos[1]] = " "
             n += 1
             current_pos = next_pos
         else:
