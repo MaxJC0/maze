7c8ff77f4063e5c3e41f439879ce593e6b9198d4
MaxM <m.mirnik@campus.tu-berlin.de>
2025-08-18 21:59:03 +0200

    Refactor maze position functions for clarity and improve maze generation logic

--- maze.py
+++ maze.py
@@ -1,10 +1,14 @@
 from random import randint
 
-def maze_pos(maze, pos):
+def pos(maze, pos):
     return (maze[pos[0]][pos[1]])
 
-def maze_pos_in(maze, pos):
-    return (pos[0] >= 1 and pos[0] < len(maze)-1 and pos[1] >= 1 and pos[1] < len(maze[0])-1)
+def pos_in(maze, pos):
+    return (pos[0] > 0 and pos[0] < len(maze)-1 and pos[1] > 0 and pos[1] < len(maze[0])-1)
+
+def path(maze, pos):
+    maze[pos[0]][pos[1]] = " "
+    return maze
 
 def create_maze(width, height):
     maze = [['█' for _ in range(width)] for _ in range(height)]
@@ -21,26 +25,32 @@ def create_maze(width, height):
         dir = directions[randint(0, 3)]
         next_pos = (current_pos[0] + dir[0], current_pos[1] + dir[1])
         goal_pos = (next_pos[0] + dir[0], next_pos[1] + dir[1])
+        # if from goal goal pos is already out choose another dir
+        if pos(maze, current_pos) == '0':
+            if not pos_in(maze, goal_pos):
+                continue
 
-        if maze_pos_in(maze, goal_pos) and maze_pos(maze, goal_pos) != '0':
-            maze[next_pos[0]][next_pos[1]] = " "
-            maze[goal_pos[0]][goal_pos[1]] = " "
+        if pos_in(maze, goal_pos):
+            if pos(maze, next_pos) == '0' or pos(maze, goal_pos) == '0':
+                continue
+            maze = path(maze, next_pos)
+            maze = path(maze, goal_pos)
             n += 2
             backtrack_stack.append(current_pos)
             current_pos = goal_pos
             backtrack_attempts = 0
-        elif maze_pos_in(maze, next_pos) and maze_pos(maze, next_pos) != '0':
-            maze[next_pos[0]][next_pos[1]] = " "
-            n += 1
-            backtrack_stack.append(current_pos)
-            current_pos = next_pos
-            backtrack_attempts = 0
+
         else:
             if backtrack_attempts < 1 and len(backtrack_stack) > 0:
                 current_pos = backtrack_stack.pop()
                 backtrack_attempts += 1
             else:
-                maze[next_pos[0]][next_pos[1]] = " "
+                print(f"current_pos: {current_pos}, next_pos: {next_pos}, goal_pos: {goal_pos} posin: {pos_in(maze, goal_pos)}")
+                if not pos_in(maze, next_pos):
+                    maze = path(maze, next_pos)
+                else:
+                    maze = path(maze, next_pos)
+                    maze = path(maze, goal_pos)
                 break
 
     return maze
@@ -52,5 +62,5 @@ def print_maze(maze):
         print()
 
 for i in range(3):
-    print_maze(create_maze(9, 9))
+    print_maze(create_maze(13, 13))
     print("\n")
