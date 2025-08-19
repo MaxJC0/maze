1a7cfbe8a6aa0d11fcdfa6b5b7744a572f9f587c
MaxM <m.mirnik@campus.tu-berlin.de>
2025-08-19 12:44:29 +0200

    Implement Maze class with methods for position handling, path carving, movement, and maze generation

--- maze.py
+++ maze.py
@@ -1,5 +1,46 @@
 from random import randint
 
+class Maze():
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.maze = [['█' for _ in range(width)] for _ in range(height)]
+        self.start = None
+        self.goal = None
+    
+    def pos(self, pos):
+        return self.maze[pos[0]][pos[1]]
+    
+    def pos_in(self, pos):
+        return (pos[0] > 0 and pos[0] < self.height-1 and pos[1] > 0 and pos[1] < self.width-1)
+
+    def path(self, pos):
+        self.maze[pos[0]][pos[1]] = " "
+        return self.maze
+    
+    def move(self, pos, direction):
+        return (pos[0] + direction[0], pos[1] + direction[1])
+
+
+    def generate(self):
+        self.goal = (randint(1, self.height - 2), randint(1, self.width - 2))
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # right, down, left, up
+        current_pos = self.goal
+        backtrack:stack = [current_pos]
+        backtrack_attempts = 0
+        while True:
+            dir = directions[randint(0, 3)]
+            next_pos = self.move(current_pos, dir)
+            goal_pos = self.move(next_pos, dir)
+            if self.pos(current_pos) == '0':
+                pass
+
+
+    def print_maze(self):
+        for row in self.maze:
+            print(" ".join(row))
+        print("\n")
+
 def pos(maze, pos):
     """
     Returns the value at the specified position in the maze.
@@ -98,6 +139,13 @@ def print_maze(maze):
             print(cell, end=' ')
         print()
 
-for i in range(3):
-    print_maze(create_maze(13, 13))
-    print("\n")
+# for i in range(3):
+#     print_maze(create_maze(13, 13))
+#     print("\n")
+
+def main():
+    maze = Maze(13, 13)
+    maze.generate()
+    maze.print_maze()
+
+main()
