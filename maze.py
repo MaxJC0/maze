5e4c3e05496dbc8a60ab31f677ec626e9c1180b7
MaxM <m.mirnik@campus.tu-berlin.de>
2025-08-27 00:22:44 +0200

    Refactor maze logic; improve user input handling and update position management

--- maze.py
+++ maze.py
@@ -8,29 +8,32 @@ class Maze():
         self.width = width
         self.height = height
         self.maze = [['█' for _ in range(width)] for _ in range(height)]
-        self.start = None
+        self.location = None
         self.goal = None
         self.directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # right, down, left, up
         self.direction_names = {
             (0, 1): "right",
             (1, 0): "down",
             (0, -1): "left",
-            (-1, 0): "up"
+            (-1, 0): "up",
+            "up": (-1, 0),
+            "down": (1, 0),
+            "left": (0, -1),
+            "right": (0, 1)
         }
 
-    def pos(self, pos):
+    def get_position_value(self, pos):
         return self.maze[pos[0]][pos[1]]
     
     def pos_in(self, pos):
         return (pos[0] > 0 and pos[0] < self.height-1 and pos[1] > 0 and pos[1] < self.width-1)
     
     def check_neighbours(self, pos): 
-        # print(f"Checking neighbours for position {pos}")
-        if self.pos(pos) != '█':
+        if self.get_position_value(pos) != '█':
             return False  # Only check for wall segments
         for direction in self.directions:
-            neighbour = self.move(pos, direction)
-            if self.pos(neighbour) == '█':
+            neighbour = self.get_new_position(pos, direction)
+            if self.get_position_value(neighbour) == '█':
                 return True
         return False
 
@@ -38,23 +41,31 @@ class Maze():
         self.maze[pos[0]][pos[1]] = " "
         return self.maze
     
-    def move(self, pos, direction):
+    def get_new_position(self, pos, direction):
         return (pos[0] + direction[0], pos[1] + direction[1])
 
     def set(self, pos, val):
         self.maze[pos[0]][pos[1]] = val
+    
+    def handleUserInput(self, user_input):
+        if user_input.lower() in ["up", "down", "left", "right"]:
+            dir = self.direction_names[user_input.lower()]
+            next_pos = self.get_new_position(self.location, dir)
+            if self.get_position_value(next_pos) == '█':
+                return
+            self.set(self.location, ' ')
+            self.location = next_pos
+            self.set(self.location, '1')
+            self.print_maze()
 
     def check_validity(self, cur, next, goal):
-        if self.pos(cur) == '0':
+        if self.get_position_value(cur) == '0':
             if not self.pos_in(goal):
-                # print(f"Goal position {goal} is out of bounds, skipping.")
                 return 2
         if self.pos_in(goal):
-            if self.pos(next) == '0' or self.pos(goal) == '0':
-                # print(f"Next position {next} or Goal position {goal} is already part of the path, skipping.")
+            if self.get_position_value(next) == '0' or self.get_position_value(goal) == '0':
                 return 2
             if not self.check_neighbours(next) or not self.check_neighbours(goal):
-                # print(f"Next position {next} or Goal position {goal} is not surrounded by walls, skipping.")
                 return 2
             return 1
         return 0
@@ -72,22 +83,19 @@ class Maze():
                 print("10 seconds timeout reached.")
                 break
 
-            # self.print_maze()
             if len(prev_dir) > 0:
                 directions = [d for d in self.directions if d not in prev_dir]
                 if len(directions) < 1:
-                    # print("No valid directions left, backtracking.")
                     if len(backtrack_stack) > 0:
                         current_pos = backtrack_stack.pop()
                     prev_dir.clear()
-                    # self.print_maze(current_pos)
                     continue
                 dir = directions[randint(0, len(directions) - 1)]
             else:
                 dir = self.directions[randint(0, 3)]
 
-            next_pos = self.move(current_pos, dir)
-            goal_pos = self.move(next_pos, dir)
+            next_pos = self.get_new_position(current_pos, dir)
+            goal_pos = self.get_new_position(next_pos, dir)
 
             validity = self.check_validity(current_pos, next_pos, goal_pos)
 
@@ -100,7 +108,6 @@ class Maze():
                 current_pos = goal_pos
                 backtrack_attempts = 0
 
-                #print(f"Moving {self.direction_names[dir]} to {current_pos}")
             
             elif validity == 2:
                 prev_dir.append(dir)
@@ -111,17 +118,15 @@ class Maze():
                     current_pos = backtrack_stack.pop()
 
                     backtrack_attempts += 1
-                    #print(f"Backtracking to {current_pos}")
                 else:
                     if not self.pos_in(next_pos):
                         self.path(next_pos)
-                        self.start = next_pos
+                        self.location = next_pos
                     else:
                         self.path(next_pos)
                         self.path(goal_pos)
-                        self.start = goal_pos
-                    self.set(self.start, '1')
-                    # print(f"Start position: {self.start}, Goal position: {self.goal} \n")
+                        self.location = goal_pos
+                    self.set(self.location, '1')
                     break
         return self.maze
     
@@ -168,7 +173,10 @@ def tutorial_maze():
     for i in range(5):
         maze.path([6 + i,5])
     maze.set([10 , 5], '1')
+    maze.goal = (5,5)
+    maze.location = (10,5)
     maze.print_maze()
+    return maze
 
 def p(text, input=False):
     printed = text
@@ -178,7 +186,7 @@ def p(text, input=False):
             printed = printed[1:]
             # time.sleep(0.02)
         print()
-        time.sleep(2)
+        # time.sleep(2)
     else:
         for char in text:
             print(char, end="", flush=True)
@@ -189,11 +197,22 @@ def tutorial():
     p("Welcome to Maze Generator!")
     p("Lets start with a tutorial.")
     p("This is you first maze to solve:")
-    tutorial_maze()
+    maze = tutorial_maze()
     p("0 is the Goal and 1 is where you start.")
     p("Try solving this maze using the 'up' command.")
-    p("Write your command: ", input=True)
-    user_input = input()
+    prev_location = maze.location
+    while prev_location == maze.location:
+        p("Write your command: ", input=True)
+        user_input = input()
+        maze.handleUserInput(user_input)
+    p("Well done!")
+    p("Now try repeating that until you reach the goal!")
+    while maze.location != maze.goal:
+        p("Write your command: ", input=True)
+        user_input = input()
+        maze.handleUserInput(user_input)
+    p("Congratulations! You reached the goal!")
+
     if user_input.lower() in ["up", "down", "left", "right"]:
         p(f"You entered: {user_input}. Great! Now try solving the maze on your own.")
 
